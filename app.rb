d8a441893127a5c21e6ddc749a1ab8684b903d49
Allegra <aukseallegra@gmail.com>
2018-06-26 09:44:06 +0100

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,19 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get "/newteam" do  #get info from "/newteam"
+    erb :newteam
+  end
+
+  post "/team" do   #post it to "/team"
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @pf = params[:pf]
+    @sf = params[:sf]
+    @c = params[:c]
+    erb :team
+  end
 
 end
